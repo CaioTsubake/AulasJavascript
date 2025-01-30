21b53773b506c731efff0f585f2604913b7a1628
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2025-01-30 19:41:20 -0300

    Comecando material sobre loops

---
+++ loops.js
@@ -0,0 +1,14 @@
+
+// loop for
+for (let index = 0; index < 5; index++) {
+    console.log("Olá mundo");
+}
+
+//loop while
+let contador = 0
+
+while (contador < 5) {
+    console.log("Olá mundo");
+
+    contador++;
+}
